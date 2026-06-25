bbff63fe239f313018a686a8ebd14ed21d8bd8c0
unknown <bissokot.joachim@gmail.com>
2026-06-25 21:19:59 +0200

    CD GIT ACTIONS

--- cron/cron_renouvellement.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (PHP_SAPI !== 'cli') {
-    http_response_code(410);
-    header('Content-Type: text/plain; charset=utf-8');
-}
-
-echo "Ce cron est désactivé. Les renouvellements sont gérés par le scheduler Laravel (php artisan schedule:run).\n";
-exit;
